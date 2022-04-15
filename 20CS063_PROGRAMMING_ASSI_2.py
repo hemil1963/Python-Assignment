cfde86171e1866cb979d66586efde77a6f44271c
hemil1963 <97402724+hemil1963@users.noreply.github.com>
2022-04-15 12:25:29 +0530

    Add files via upload

---
+++ 20CS063_PROGRAMMING_ASSI_2.py
@@ -0,0 +1,8 @@
+'''PREPARED BY-20CS063'''
+numbers = list(map(int,input("Enter numbers : ").split())) import numpy
+import statistics
+
+def Average(lst):
+return sum(lst) / len(lst) print("Minimum : " + str(min(numbers)))
+print("Maximum : " + str(max(numbers))) print("Average : " + str(Average(numbers)))
+print("Standard Deviation : " + str(statistics.stdev(numbers))) print("Vartiance : " + str(numpy.var(numbers)))
---
+++ 20CS063_PROGRAMMING_ASSI_3.py
@@ -0,0 +1,9 @@
+'''PREPARED BY-20CS063'''
+heights = list(map(int,input("Enter numbers : ").split())) maxh = 0
+for i in heights: if i > maxh:
+maxh=i
+
+
+maxh2 = 0 heights.pop(heights.index(maxh)) for i in heights:
+if i > maxh2:
+maxh2=i print(maxh2*maxh2)
